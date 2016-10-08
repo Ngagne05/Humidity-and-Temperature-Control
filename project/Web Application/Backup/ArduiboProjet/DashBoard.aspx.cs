14f5fe3053443c0ccba88d935ce8affde63fe0d8
Ngagne05 <kuta_92@yahoo.fr>
2016-10-08 23:28:13 +0800

    sensor project

---
+++ project/Web Application/Backup/ArduiboProjet/DashBoard.aspx.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Globalization;
+using System.Threading;
+
+namespace ArduiboProjet
+{
+    public partial class DashBoard : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                printValue();
+
+                Timer1.Enabled = true;
+
+            }
+        }
+
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            printValue();
+        }
+
+        //Print the value of température humidity
+        protected void printValue()
+        {
+            string[] param = new Query().getParam();
+            string lastTemp = new Query().getTempFromDb();
+            string lastHumid = new Query().getHUMDFromDb();
+
+            if (lastHumid == "")
+            {
+                lastHumid = "0";
+            }
+
+            if (lastTemp == "")
+            {
+                lastTemp = "0";
+            }
+
+
+            lblTemp.Text = lastTemp + "°C";
+            lblHumid.Text = lastHumid + "%";
+
+            if (float.Parse(param[0], CultureInfo.InvariantCulture.NumberFormat) < float.Parse(lastTemp, CultureInfo.InvariantCulture.NumberFormat))
+            {
+                lblTemp.ForeColor = System.Drawing.Color.DarkRed;
+            }
+            else
+            {
+                lblTemp.ForeColor = System.Drawing.Color.DarkGreen;
+            }
+
+            if (float.Parse(param[2], CultureInfo.InvariantCulture.NumberFormat) < float.Parse(lastHumid, CultureInfo.InvariantCulture.NumberFormat))
+            {
+                lblHumid.ForeColor = System.Drawing.Color.DarkRed;
+            }
+            else
+            {
+                lblHumid.ForeColor = System.Drawing.Color.DarkGreen;
+            }
+        }
+
+
+        public void saveTemperatureHumidity()
+        {
+            while (Query.stopGettingData == false)
+            {
+                if (Query.getPortName().Length > 0)
+                {
+                    System.IO.Ports.SerialPort com1 = new System.IO.Ports.SerialPort(Query.getPortName()[0]);
+                    string valueTempHumd = String.Empty;
+                    string temp = string.Empty;
+                    string humd = string.Empty;
+                    //string[] valueTsend = getParam();
+
+                    try
+                    {
+                        com1.Open();
+                        com1.BaudRate = 9600;
+                        valueTempHumd = (String)com1.ReadLine();
+                        if ((valueTempHumd != "") && (valueTempHumd.Contains(";")))
+                        {
+                            char[] delimiters = new char[] { ';' };
+                            temp = valueTempHumd.Split(delimiters, StringSplitOptions.RemoveEmptyEntries)[0];
+                            humd = valueTempHumd.Split(delimiters, StringSplitOptions.RemoveEmptyEntries)[1];
+
+                            Query dataManager = new Query();
+                            dataManager.thingSpeak(temp, humd);// Using thingSpeak api
+                            dataManager.Insert(temp, humd);//Insert data in database
+                        }
+                        com1.Close();
+                    }
+                    catch (TimeoutException ex)
+                    {
+                        lblStartInfo.Visible = true;
+                        lblStartInfo.Text = "Failure. Check Ports";
+                    }
+                }
+                else
+                {
+
+                }
+
+            }
+
+
+        }
+
+        protected void ImgStart_Click(object sender, ImageClickEventArgs e)
+        {
+            Query.stopGettingData = false;
+            Query.readThread = new Thread(saveTemperatureHumidity);
+            Query.readThread.Start();
+            lblStartInfo.Visible = false;
+        }
+
+        protected void ImgStop_Click(object sender, ImageClickEventArgs e)
+        {
+            Query.stopGettingData = true;
+            if (Query.readThread.IsAlive)
+            {
+                Query.readThread.Join();
+            }
+        }
+
+        protected void ImageButton1_Click(object sender, ImageClickEventArgs e)
+        {
+            string[] portNames = Query.getPortName();
+
+            Query.readThread.Join();
+
+            // Control if there is opened port
+            if (portNames.Length > 0)
+            {
+                System.IO.Ports.SerialPort com1 = new System.IO.Ports.SerialPort(portNames[0]);
+                string valueTempHumd = String.Empty;
+                string temp = string.Empty;
+                string humd = string.Empty;
+
+                try
+                {
+                    com1.Open();
+                    com1.BaudRate = 9600;
+
+                    com1.WriteLine("1");
+                    com1.Close();
+                }
+                catch (TimeoutException ex)
+                {
+                    lblPush.Text = "Operation failed. Port is closed";
+                }
+            }
+            else
+            {
+                lblPush.Text = "No ports found.";
+            }
+        }
+    }
+}
---
+++ project/Web Application/Backup/ArduiboProjet/Query.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MySql.Data.MySqlClient;
+using System.Net;
+using System.Threading;
+
+namespace ArduiboProjet
+{
+    public class Query
+    {
+        public MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        public static bool stopGettingData { get; set; }
+        public static Thread readThread;
+        //Constructor
+        public Query()
+        {
+            Initialize();
+        }
+
+        //Initialize values
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "arduinoprojet";
+            uid = "admin";
+            password = "admin";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        //open connection to database
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                //When handling errors, you can your application's response based 
+                //on the error number.
+                //The two most common error numbers when connecting are as follows:
+                //0: Cannot connect to server.
+                //1045: Invalid user name and/or password.
+                switch (ex.Number)
+                {
+                    case 0:
+
+                        break;
+
+                    case 1045:
+
+                        break;
+                }
+                return false;
+            }
+        }
+
+        //Close connection
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                return false;
+            }
+        }
+
+        //Insert statement
+        public void Insert(string pTemp, string pHumd)
+        {
+            DateTime thisDay = DateTime.Today;
+            string query = "Insert into temphumd(HUMD,TEMP,DATE) values ('" + pHumd + "','" + pTemp + "','" + thisDay + "')";
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        //Update statement
+        public void Update()
+        {
+        }
+
+        //Delete statement
+        public void Delete()
+        {
+        }
+
+        //Backup
+        public void Backup()
+        {
+        }
+
+        //Restore
+        public void Restore()
+        {
+        }
+
+        // Get the last data of temperature in the database
+        public string getTempFromDb()
+        {
+            string result = string.Empty;
+            string selectTempQuery = "SELECT TEMP FROM temphumd WHERE ID=(SELECT MAX(ID) FROM temphumd)";
+            MySqlCommand sqlCommand;
+            MySqlDataReader SqlReader;
+
+            if (this.OpenConnection() == true)
+            {
+                sqlCommand = new MySqlCommand(selectTempQuery, connection);
+                SqlReader = sqlCommand.ExecuteReader();
+
+                while (SqlReader.Read())
+                {
+                    result = SqlReader[0].ToString();
+                }
+                CloseConnection();
+            }
+
+            return result;
+        }
+
+        //Get the parameters values
+        public string[] getParam()
+        {
+            string[] result = new string[4];
+            string selectTempQuery = "SELECT tmax,tmin,hmax,hmin FROM param";
+            MySqlCommand sqlCommand;
+            MySqlDataReader SqlReader;
+
+
+            if (this.OpenConnection() == true)
+            {
+                sqlCommand = new MySqlCommand(selectTempQuery, connection);
+                SqlReader = sqlCommand.ExecuteReader();
+
+                while (SqlReader.Read())
+                {
+                    result[0] = SqlReader["tmax"].ToString();
+                    result[1] = SqlReader["tmin"].ToString();
+                    result[2] = SqlReader["hmax"].ToString();
+                    result[3] = SqlReader["hmin"].ToString();
+                }
+                CloseConnection();
+            }
+
+            return result;
+        }
+
+        public void updateSetting(string pTpmax, string pTpmin, string pHmax, string pHmin)
+        {
+            string query = "UPDATE param SET tmax = '" + pTpmax + "', tmin ='" + pTpmin + "', hmax ='" + pHmax + "', hmin ='" + pHmin + "' WHERE ID = 1";
+
+            //open connection
+            if (this.OpenConnection() == true)
+            {
+                //create command and assign the query and connection from the constructor
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+
+                //Execute command
+                cmd.ExecuteNonQuery();
+
+                //close connection
+                this.CloseConnection();
+            }
+        }
+
+        // Get the name of the opened ports
+        public static string[] getPortName()
+        {
+            string[] portName = System.IO.Ports.SerialPort.GetPortNames();
+            return portName;
+        }
+
+        //Get the humidity from the database
+        public string getHUMDFromDb()
+        {
+            string result = string.Empty;
+            string selectTempQuery = "SELECT HUMD FROM temphumd WHERE ID=(SELECT MAX(ID) FROM temphumd)";
+            MySqlCommand sqlCommand;
+            MySqlDataReader SqlReader;
+
+            if (this.OpenConnection() == true)
+            {
+                sqlCommand = new MySqlCommand(selectTempQuery, connection);
+                SqlReader = sqlCommand.ExecuteReader();
+
+                while (SqlReader.Read())
+                {
+                    result = SqlReader[0].ToString();
+                }
+                CloseConnection();
+            }
+
+            return result;
+        }
+
+
+        //Using of thingSpeak api to send data online
+        public void thingSpeak(string temp, string humid)
+        {
+            try
+            {
+
+                const string WRITEKEY = "IE12SVCQ9S9E1N97";
+                string strUpdateBase = "http://api.thingspeak.com/update";
+                string strUpdateURI = strUpdateBase + "?key=" + WRITEKEY;
+                string strField1 = temp;
+                string strField2 = humid;
+                HttpWebRequest ThingsSpeakReq;
+                HttpWebResponse ThingsSpeakResp;
+
+                strUpdateURI += "&field1=" + strField1;
+                strUpdateURI += "&field2=" + strField2;
+
+                ThingsSpeakReq = (HttpWebRequest)WebRequest.Create(strUpdateURI);
+
+                ThingsSpeakResp = (HttpWebResponse)ThingsSpeakReq.GetResponse();
+
+                if (!(string.Equals(ThingsSpeakResp.StatusDescription, "OK")))
+                {
+                    Exception exData = new Exception(ThingsSpeakResp.StatusDescription);
+                    throw exData;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
